12d0de0c8fc17fd6fc2b94837246697ae64edef1
Eddahkarambu <gikunaeddah@gmail.com>
2021-06-11 15:46:19 +0300

    added days and names of people

--- js/scripts.js
+++ js/scripts.js
@@ -28,4 +28,64 @@ function determineAkanName(day,month,year,gender){
   var dayOfTheWeek= (((century/4)-2*century-1)+((5*centure/4) )+((26*(month+1)/10))+day)%7;
   var akanDay=(parseInt(dayOfTheWeek));
 
-}
+  if(akanDay===0){
+    if(gender ==="male"){
+      alert("your akan name is Kwasi")
+
+      }
+      else {
+        alert("your name is Akosua")
+      }
+  }
+
+  if(akanDay === 1){
+    if (gender==="male"){
+      alert("your akan name is kwadwo")
+
+    }
+    else{
+      alert("your name is Adwo")
+    }
+
+  }
+
+  if(akanDay ===2){
+    if(gender === "male"){
+      alert("your akan name is kwabena")
+    }
+    else {
+      alert("your akan name is Abenaa")
+    }
+  }
+  if(akanDay ===3){
+    if(gender === "male"){
+      alert("your akan name is kwaku")
+    }
+    else {
+      alert("your akan name is Akua")
+    }
+  }
+  if(akanDay ===4){
+    if(gender === "male"){
+      alert("your akan name is Yaw")
+    }
+    else {
+      alert("your akan name is yaa")
+    }
+  }
+  if(akanDay ===5){
+    if(gender === "male"){
+      alert("your akan name is kofi")
+    }
+    else {
+      alert("your akan name is afua")
+    }
+  }
+  if(akanDay ===6){
+    if(gender === "male"){
+      alert("your akan name is kwame")
+    }
+    else {
+      alert("your akan name is Ama")
+    }
+  }
