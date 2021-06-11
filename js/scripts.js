e5db530c521426316263c58e4d73099516a490f1
Eddahkarambu <gikunaeddah@gmail.com>
2021-06-11 15:07:21 +0300

    added afew styles

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,23 @@ function validateInput(){
     alert("please enter the correct month")
   }
   var year = document.getElementById('year').value;
-  console.log(year)
+
   if(year>2021){
     alert("kindly enter the correct year")
   }
+  var gender= document.querySelector('input[name = gender]:checked');
+  if(!gender){
+    alert("please select your gender")
+  }
+  gender= gender.value;
+  determineAkanName(parseInt(dateOfBirth),parseInt(month),year,gender);
+}
+
+function determineAkanName(day,month,year,gender){
+  var century = parseInt(year.slice(0,2));
+  var centure = parseInt(year.slice(2,4));
+
+  var dayOfTheWeek= (((century/4)-2*century-1)+((5*centure/4) )+((26*(month+1)/10))+day)%7;
+  var akanDay=(parseInt(dayOfTheWeek));
+
 }
